9c2dadf69310dc0d76992ed12901c35fc3eeb434
RAVINTHIRARAJA THUVARAKAN <139629503+rayuthuva05@users.noreply.github.com>
2025-10-13 08:59:59 +0530

    Update icae2.py

--- icae2.py
+++ icae2.py
@@ -1,1 +1,9 @@
-import 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+data=pd.read_csv("customer_behavior_dataset.csv)
+
+for val in data:
+   print(val)
